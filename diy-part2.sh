4acfb82c56e39484a3d7d04c6a5ca3170f64ca29
lefty <lefty1982@gmail.com>
2025-05-17 12:50:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,6 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# 移除USB网络共享
+sed -i 's/kmod-usb-net-rndis //g' target/linux/mediatek/image/mt7986.mk
